02c5bf762379f4bd2ea8befb782c8f129cdab221
VeraEleanor <63585873+VeraEleanor@users.noreply.github.com>
2023-03-20 21:39:02 +0100

    trying to fix routes

--- Backend/Controllers/collectionController.js
+++ Backend/Controllers/collectionController.js
@@ -1,4 +1,3 @@
-const { application } = require('express');
 const db = require('../db.js');
 
 //get all artists
@@ -56,7 +55,7 @@ exports.getArtistById = (req, res) => {
 exports.createArtist = (req, res) => { 
     const query  = 'INSERT INTO artists (Name) VALUES (@name)'
         const { name } = req.body;
-        db.pool.request().query(query, (Err, result) => {
+        db.pool.request().query(query, (err, result) => {
           if (err) {
             console.log('Error executing query:', err);
             res.status(500).send('Error executing query');
@@ -72,10 +71,9 @@ exports.createArtist = (req, res) => {
       };
       
 //delete artist by id
-exports.deleteArtistById = (req, res) => {
+exports.deleteArtist = (req, res) => {
   const query = 'DELETE FROM Artists WHERE id = ?'  
   const { id } = req.params;
-  const { name } = req.body;
   db.pool.request().query(query, (err, result) => {
     if (err) { 
       console.log('Error executing query:', err);
@@ -86,16 +84,16 @@ exports.deleteArtistById = (req, res) => {
         }).join('\n');
         res.set('Content-Type', 'application/json');
         res.send(jsonResult);
+        res.send(`Artist with id ${id} has been deleted from the database`);
       }
   }
   );
 };
 
 //delete album by id
-exports.deleteAlbumById = (req, res) => {
+exports.deleteAlbum = (req, res) => {
   const query = 'DELETE FROM Albums WHERE id = ?'
   const { id } = req.params;
-  const { name } = req.body;
   db.pool.request().query(query, (err, result) => {
     if (err) { 
       console.log('Error executing query:', err);
@@ -106,6 +104,51 @@ exports.deleteAlbumById = (req, res) => {
         }).join('\n');
         res.set('Content-Type', 'application/json');
         res.send(jsonResult);
+        res.send(`Album with ${id} has been deleted from the database`);
+      }
+  }
+  );
+};
+
+//update artist by id
+exports.updateArtist = (req, res) => {
+  const query = 'UPDATE Artists SET Name = ? WHERE id = ?'
+  const { id } = req.params;
+  const { name } = req.body;
+  db.pool.request().query(query, (err, result) => {
+    if (err) { 
+      console.log('Error executing query:', err);
+      res.status(500).send('Error executing query');
+    } else {
+      const jsonResult = result.recordset.map((record) => {
+          return JSON.stringify(record) + '\n';
+        }
+        ).join('\n');
+        res.set('Content-Type', 'application/json');
+        res.send(jsonResult);
+        res.send(`Artist with id ${id} has been updated`);
+      }
+  }
+  );
+};
+
+//update album by id
+exports.updateAlbum = (req, res) => {
+  const query = 'UPDATE Albums SET Title = ?, ArtistId = ?, ComposerId = ? WHERE id = ?'
+  const { id } = req.params;
+  const { title, artistId, composerId } = req.body;
+  db.pool.request().query(query, (err, result) => {
+    if (err) { 
+      console.log('Error executing query:', err);
+      res.status(500).send('Error executing query');
+    } else {
+      const jsonResult = result.recordset.map((record) => {
+          return JSON.stringify(record) + '\n';
+        }
+        ).join('\n');
+        res.set('Content-Type', 'application/json');
+        res.send(jsonResult);
+        res.send(`Album with id ${id} has been updated`);
       }
   }
   );
