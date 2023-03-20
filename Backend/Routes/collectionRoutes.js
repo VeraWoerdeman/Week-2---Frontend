e6280a2df046b5202c3d8ffdbafa2a2e39dca005
VeraEleanor <63585873+VeraEleanor@users.noreply.github.com>
2023-03-20 22:24:59 +0100

    routes fixed and working

--- Backend/Routes/collectionRoutes.js
+++ Backend/Routes/collectionRoutes.js
@@ -1,16 +1,21 @@
+const collectionController = require('../Controllers/collectionController');
 const express = require('express');
-const collectionController = require ('../Controllers/collectionController.js')
-const router = express.Router(); 
+const router = express.Router();
 
+router.get('/artists', collectionController.getAllArtists);
 router.get('/albums', collectionController.getAllAlbums);
-router.get('/artists', collectionController.getAllArtists)
-router.get('/artists/:id', collectionController.getArtistById);
+
 router.get('/albums/:id', collectionController.getAlbumById);
-router.put('/artists/:id', collectionController.updateArtistById);
-router.put('/albums/:id', collectionController.updateAlbumById);
-router.delete('/artists/:id', collectionController.deleteArtistById);
-router.delete('/albums/:id', collectionController.deleteAlbumById);
-router.post('/albums/:id', collectionController.createAlbumById);
-router.post('/artists/:id', collectionController.createArtistById);
+router.get('/artists/:id', collectionController.getArtistById);
+
+router.post('/albums', collectionController.createAlbum);
+router.post('/artists', collectionController.createArtist)
+
+// router.put('/album/:id', collectionController.updateAlbum);
+// router.put('/artist/:id', collectionController.updateArtist);
+
+// router.delete('/album/:id', collectionController.deleteAlbum);
+// router.delete('/artist/:id', collectionController.deleteArtist);
 
 module.exports = router;
+
